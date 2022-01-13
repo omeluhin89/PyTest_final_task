2cf345027847b82eef3403447278b9e972050e0c
omelyuhin <omeluhin89@gmail.com>
2022-01-13 22:41:46 +0200

    write your message

---
+++ conftest.py
@@ -0,0 +1,30 @@
+import time
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru or en")
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    language = request.config.getoption("language")
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox" and language is True:
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    yield browser
+    print("\nquit browser..")
+    time.sleep(5)
+    browser.quit()
